ed31566b453ca16ca6c03819a0bb9fc35a157403
romfantast <romfantast@gmail.com>
2022-11-14 19:29:04 +0200

    done

--- src /js/01-gallery.js
+++ src /js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map((photo) => {
+      return `
+			<div class="gallery__item">
+  				<a class="gallery__link" href="${photo.original}">
+    			<img
+      class="gallery__image"
+      src="${photo.preview}"
+      data-source="${photo.original}"
+      alt="${photo.description}"
+    />
+  </a>
+</div>
+		`;
+    })
+    .join("");
+}
+
+galleryList.innerHTML = createGalleryMarkup(galleryItems);
+
+galleryList.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("click");
+
+  if (e.target.nodeName !== "IMG") {
+  }
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  galleryList.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  });
+});
--- src /js/02-lightbox.js
+++ src /js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map((photo) => {
+      return `
+			<a class="gallery__item" href="${photo.original}">
+  <img class="gallery__image" src="${photo.preview}" alt="${photo.description}" />
+</a>
+		`;
+    })
+    .join("");
+}
+
+galleryList.innerHTML = createGalleryMarkup(galleryItems);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 300,
+  animationSpeed: 150,
+  fadeSpeed: 150,
+});
